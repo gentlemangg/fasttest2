4d385a9f18b5bd53389a3bcce3b4a8e0d69cdaeb
gentlemangg <120128234+gentlemangg@users.noreply.github.com>
2022-12-09 06:41:01 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=13ZqR1Ny2VPiid7gZ6n1IXPW9H3Azoxpc'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1r-mDl959X6AqO8UGzltpIuznSQFLtgAp'
 export_file_name = 'export.pkl'
 
 classes = ['blumeana', 'moso', 'quadrangularis']
