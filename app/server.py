a1116bb378df5c204b1b1a61f3f3af54b86c8bc4
gentlemangg <120128234+gentlemangg@users.noreply.github.com>
2022-12-09 06:46:25 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 import aiohttp
 import asyncio
 import uvicorn
 from fastai import *
 from fastai.vision import *
+from fastai.vision.all import *
 from io import BytesIO
 from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
@@ -10,7 +12,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 export_file_url = 'https://drive.google.com/uc?export=download&id=1r-mDl959X6AqO8UGzltpIuznSQFLtgAp'
-export_file_name = 'export.pkl'
+export_file_name = 'stem3.pkl'
 
 classes = ['blumeana', 'moso', 'quadrangularis']
 path = Path(__file__).parent
@@ -59,7 +61,7 @@ async def homepage(request):
 async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
+    img = load_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
 
